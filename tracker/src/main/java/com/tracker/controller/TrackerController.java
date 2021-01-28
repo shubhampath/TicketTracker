6fe522bd3589da428b50ea97f72c000a521addf9
shubham <shubhampathrikar@yahoo.in>
2021-01-28 19:08:50 +0530

    second commit

--- tracker/src/main/java/com/tracker/controller/TrackerController.java
+++ tracker/src/main/java/com/tracker/controller/TrackerController.java
@@ -1,7 +1,29 @@
 package com.tracker.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.tracker.entity.User;
 
 public class TrackerController implements ITrackerController {
-
+	
+	@RequestMapping("/")
+	public String getLogin(Model theModel) {
+		
+		User user = User.builder().build();
+		theModel.addAttribute("user", user);
+		
+		return "login";
+	}
+	
+	@RequestMapping("/user")
+	public String getUser(@ModelAttribute("user") User theUser, Model theModel){
+		theModel.addAttribute("username", theUser.getUsername());
+		theModel.addAttribute("password", theUser.getPassword());
+		System.out.println(theUser.getUsername());
+		System.out.println(theUser.getPassword());
+		
+		return "user";
+	}
 }
--- tracker/src/main/java/com/tracker/entity/User.java
+++ tracker/src/main/java/com/tracker/entity/User.java
@@ -1,13 +1,18 @@
 package com.tracker.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
 	private String firstName;
 	private String lastName;
+	private String username;
 	private String email;
 	private String password;
 	private String userType;
